2fef53c66385b4fd6a10f2ba2d11d14573c6885c
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-15 19:49:06 +0300

    Task2

--- src/main/java/edu/hw1/Task2.java
+++ src/main/java/edu/hw1/Task2.java
@@ -1,2 +1,17 @@
-package edu.hw1;public class Task2 {
+package edu.hw1;
+
+public class Task2 {
+    private static final int RADIX = 10;
+
+    public static int CountDigits(int num){
+        if (num ==0){
+            return 1;
+        }
+        int count = 0;
+        while (num > 0){
+            num /= RADIX;
+            count += 1;
+        }
+        return count;
+    }
 }
--- src/test/java/edu/hw1/Task2Test.java
+++ src/test/java/edu/hw1/Task2Test.java
@@ -1,2 +1,59 @@
-package edu.hw1;public class Task2Test {
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task2Test {
+
+    @Test
+    @DisplayName("11")
+    void Test11() {
+        int num = 11;
+
+        int ans = Task2.CountDigits(num);
+
+        assertThat(ans)
+            .isEqualTo(2);
+    }
+    @Test
+    @DisplayName("0")
+    void Test0() {
+        int num = 0;
+
+        int ans = Task2.CountDigits(num);
+
+        assertThat(ans)
+            .isEqualTo(1);
+    }
+    @Test
+    @DisplayName("100")
+    void Test100() {
+        int num = 100;
+
+        int ans = Task2.CountDigits(num);
+
+        assertThat(ans)
+            .isEqualTo(3);
+    }
+    @Test
+    @DisplayName("1234")
+    void Test1234() {
+        int num = 1234;
+
+        int ans = Task2.CountDigits(num);
+
+        assertThat(ans)
+            .isEqualTo(4);
+    }
+    @Test
+    @DisplayName("max")
+    void Testmax() {
+        int num = Integer.MAX_VALUE;
+
+        int ans = Task2.CountDigits(num);
+
+        assertThat(ans)
+            .isEqualTo(10);
+    }
 }
