c545b56f6eb46fd742fffeac0b90ecc822f9bd61
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-15 21:23:00 +0300

    исправляю Task1

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -3,8 +3,6 @@ package edu.hw1;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public final class Task0 {
     private final static Logger LOGGER = LogManager.getLogger();
 
@@ -12,11 +10,6 @@ public final class Task0 {
     }
 
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         LOGGER.info("Привет, мир!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-
     }
 }
--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -2,15 +2,19 @@ package edu.hw1;
 
 import org.jetbrains.annotations.NotNull;
 
-public class Task1{
+public class Task1 {
     private static final int SECS_IN_MIN = 60;
-    public static int Mins_to_secs(@NotNull String timeinmins){
-        if (timeinmins.matches("[0-9]+:[0-5][0-9]")){
+
+    private Task1() {
+    }
+
+    public static int Mins_to_secs(@NotNull String timeinmins) {
+        if (timeinmins.matches("[0-9]+:[0-5][0-9]")) {
             String[] timearray = timeinmins.split(":");
-            try{
+            try {
                 return Integer.parseInt(timearray[0]) * SECS_IN_MIN + Integer.parseInt(timearray[1]);
             }
-            catch (NumberFormatException e){
+            catch (NumberFormatException e) {
                 return -1;
             }
         }
