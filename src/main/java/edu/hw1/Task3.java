ae0451db80ff59264287c4656b4a27ae0cc4452a
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-18 20:57:45 +0300

    Исправляю Task1 - 4

--- src/main/java/edu/hw1/Task3.java
+++ src/main/java/edu/hw1/Task3.java
@@ -10,8 +10,7 @@ public class Task3 {
     public static boolean isNestable(int[] array1, int[] array2) {
         if (array1.length == 0) {
             return true;
-        }
-        else if (array2.length == 0) {
+        } else if (array2.length == 0) {
             return false;
         }
         return (Arrays.stream(array1).min().getAsInt() > Arrays.stream(array2).min().getAsInt())
