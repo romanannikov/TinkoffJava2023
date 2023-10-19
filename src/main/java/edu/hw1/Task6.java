a4f89e6f307180a37eba45cf35f7ba4ebeda2829
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-19 03:12:02 +0300

    Исправляю Task6

--- src/main/java/edu/hw1/Task6.java
+++ src/main/java/edu/hw1/Task6.java
@@ -9,22 +9,24 @@ public class Task6 {
     }
 
     private final static int CONST_KAPR = 6174;
+    private final static int LEN = 4;
+    private final static int RADIX = 10;
 
     public static int funcKapr(@NotNull Integer num) {
         int minNum = 0;
         int maxNum = 0;
         char[] tempArray = num.toString().toCharArray();
-        int[] numArray = new int[4];
-        for (int i = 0; i < 4; i++) {
+        int[] numArray = new int[LEN];
+        for (int i = 0; i < LEN; i++) {
             numArray[i] = tempArray[i] - '0';
             if ((i > 0) && (numArray[i] == numArray[i - 1])) {
                 return -1;
             }
         }
         Arrays.sort(numArray);
-        for (int i = 0; i < 4; i++) {
-            minNum = minNum * 10 + numArray[i];
-            maxNum = maxNum * 10 + numArray[4 - i - 1];
+        for (int i = 0; i < LEN; i++) {
+            minNum = minNum * RADIX + numArray[i];
+            maxNum = maxNum * RADIX + numArray[LEN - i - 1];
         }
         Integer residual = maxNum - minNum;
         if (residual.equals(CONST_KAPR)) {
