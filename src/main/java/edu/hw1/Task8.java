f3986a3691a6bb4b7d2f886bd57ff19f79ac177c
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-20 22:07:57 +0300

    Исправил тесты и сделал Task8

---
+++ src/main/java/edu/hw1/Task8.java
@@ -0,0 +1,27 @@
+package edu.hw1;
+
+public class Task8 {
+
+    private final static int[][] POSSIBLE_MOVES =
+        {{1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
+    private final static int SIZE = 8;
+
+    private Task8() {
+    }
+
+    public static boolean knightBoardCapture(int[][] field) {
+        for (int y = 0; y < SIZE; y++) {
+            for (int x = 0; x < SIZE; x++) {
+                if (field[x][y] == 1) {
+                    for (var move : POSSIBLE_MOVES) {
+                        if (x + move[0] >= 0 && x + move[0] < SIZE && y + move[1] >= 0 && y + move[1] < SIZE &&
+                            field[x + move[0]][y + move[1]] == 1) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
--- src/test/java/edu/hw1/Task3Test.java
+++ src/test/java/edu/hw1/Task3Test.java
@@ -9,7 +9,7 @@ public class Task3Test {
     @Test
     @DisplayName("[1, 2, 3, 4], [0, 6]")
     void test1() {
-        int [] array1 = {1, 2, 3, 4};
+        int[] array1 = {1, 2, 3, 4};
         int[] array2 = {0, 6};
         boolean ans = Task3.isNestable(array1, array2);
 
@@ -19,7 +19,7 @@ public class Task3Test {
     @Test
     @DisplayName("[3, 1], [4, 0]")
     void test2() {
-        int [] array1 = {3, 1};
+        int[] array1 = {3, 1};
         int[] array2 = {4, 0};
         boolean ans = Task3.isNestable(array1, array2);
 
@@ -29,7 +29,7 @@ public class Task3Test {
     @Test
     @DisplayName("[9, 9, 8], [8, 9]")
     void test3() {
-        int [] array1 = {9, 9, 8};
+        int[] array1 = {9, 9, 8};
         int[] array2 = {8, 9};
         boolean ans = Task3.isNestable(array1, array2);
 
@@ -39,7 +39,7 @@ public class Task3Test {
     @Test
     @DisplayName("[], [0, 6]")
     void test4() {
-        int [] array1 = {};
+        int[] array1 = {};
         int[] array2 = {0, 6};
         boolean ans = Task3.isNestable(array1, array2);
 
@@ -49,7 +49,7 @@ public class Task3Test {
     @Test
     @DisplayName("[1, 2, 3, 4], []")
     void test5() {
-        int [] array1 = {1, 2, 3, 4};
+        int[] array1 = {1, 2, 3, 4};
         int[] array2 = {};
         boolean ans = Task3.isNestable(array1, array2);
 
