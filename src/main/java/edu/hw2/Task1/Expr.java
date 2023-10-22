b092c2c0f92f2dede3bb6b4720fa75b02c0f1c38
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-22 03:05:39 +0300

    Исправляю Task1

--- src/main/java/edu/hw2/Task1/Expr.java
+++ src/main/java/edu/hw2/Task1/Expr.java
@@ -3,28 +3,32 @@ package edu.hw2.Task1;
 public sealed interface Expr permits Expr.Constant, Expr.Negate, Expr.Exponent, Expr.Addition, Expr.Multiplication {
     double evaluate();
 
-    public record Constant (double num) implements Expr {
+    record Constant(double num) implements Expr {
         @Override
         public double evaluate() {
             return num;
         }
     }
-    public record Negate (Expr num) implements Expr {
+
+    record Negate(Expr num) implements Expr {
         public double evaluate() {
             return num.evaluate() * (-1);
         }
     }
-    public record Exponent (Expr num, double exp) implements Expr {
+
+    record Exponent(Expr num, double exp) implements Expr {
         public double evaluate() {
             return Math.pow(num.evaluate(), exp);
         }
     }
-    public record Addition (Expr fNum, Expr sNum) implements Expr {
+
+    record Addition(Expr fNum, Expr sNum) implements Expr {
         public double evaluate() {
             return fNum.evaluate() + sNum.evaluate();
         }
     }
-    public record Multiplication (Expr fNum, Expr sNum) implements Expr {
+
+    record Multiplication(Expr fNum, Expr sNum) implements Expr {
         public double evaluate() {
             return fNum.evaluate() * sNum.evaluate();
         }
