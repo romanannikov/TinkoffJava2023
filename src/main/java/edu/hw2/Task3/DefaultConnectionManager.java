fa0ad0d723a397e7e9375135d955f28c40eebbc8
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-24 01:03:58 +0300

    Исправляю Checkstyle

--- src/main/java/edu/hw2/Task3/DefaultConnectionManager.java
+++ src/main/java/edu/hw2/Task3/DefaultConnectionManager.java
@@ -2,10 +2,10 @@ package edu.hw2.Task3;
 
 public class DefaultConnectionManager implements ConnectionManager {
 
-    private double MANAGERFAILPROP;
+    private static final double MANAGER_FAIL_PROP = 0.25;
 
     @Override
     public Connection getConnection() {
-        return Math.random() < MANAGERFAILPROP ? new StableConnection() : new FaultyConnection();
+        return Math.random() < MANAGER_FAIL_PROP ? new StableConnection() : new FaultyConnection();
     }
 }
