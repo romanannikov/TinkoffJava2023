16d9abff18944a3f4c8a2d50e2fa021dce02382d
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-29 22:39:33 +0300

    Исправляю Task7

--- src/main/java/edu/hw3/Task2.java
+++
@@ -1,2 +0,0 @@
-package edu.hw3;public class Task2 {
-}
--- src/test/java/edu/hw1/Task7Test.java
+++ src/test/java/edu/hw1/Task7Test.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Task7Test {
-    @ParameterizedTest(name = "{index}: {0} -{1}-> {2}")
+    @ParameterizedTest(name = "{index}: {0} --{1}--> {2}")
     @CsvSource({"16, 1, 1", "17, 2, 6"})
     void testLeft(int num, int shift, int expRotatedNum) {
 
@@ -13,7 +13,7 @@ public class Task7Test {
         assertEquals(rotatedNum, expRotatedNum);
     }
 
-    @ParameterizedTest(name = "{index}: {0} -{1}-> {2}")
+    @ParameterizedTest(name = "{index}: {0} --{1}--> {2}")
     @CsvSource({"16, 2, 4", "8, 1, 4"})
     void testRight(int num, int shift, int expRotatedNum) {
 
