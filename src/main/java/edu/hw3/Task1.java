5fb6e3c100ee2bef57e056cfe9190d2eefb4a77f
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-29 22:17:21 +0300

    Сделал Task1

---
+++ src/main/java/edu/hw3/Task1.java
@@ -0,0 +1,25 @@
+package edu.hw3;
+
+public class Task1 {
+
+    private static final int SIZE = 'z' - 'a';
+    private Task1() {
+    }
+
+    public static String atbash(String word) {
+        char[] wordArray = word.toCharArray();
+        StringBuilder encodedWord = new StringBuilder();
+        for (char tempChar : wordArray) {
+            if ('a' <= tempChar && tempChar <= 'z') {
+                encodedWord.append((char) ('a' + SIZE - (tempChar - 'a')));
+            } else if ('A' <= tempChar && tempChar <= 'Z') {
+                encodedWord.append((char) ('A' + SIZE - (tempChar - 'A')));
+            } else {
+                encodedWord.append(tempChar);
+            }
+        }
+        return encodedWord.toString();
+    }
+
+
+}
