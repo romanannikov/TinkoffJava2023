56f650b92c8d962d135f456a99355d849fcd7b6b
romanannikov <45354358+romanannikov@users.noreply.github.com>
2023-10-30 02:39:38 +0300

    Сделал Task3

--- src/main/java/edu/hw3/Task3.java
+++ src/main/java/edu/hw3/Task3.java
@@ -2,14 +2,13 @@ package edu.hw3;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Objects;
 
 public class Task3 {
 
     private Task3() {
     }
 
-    public static String freqDict (ArrayList<Object> wordsArray) {
+    public static String freqDict(ArrayList<Object> wordsArray) {
         HashMap<Object, Integer> dict = new HashMap<>();
         for (Object tempObj : wordsArray) {
             if (dict.containsKey(tempObj)) {
